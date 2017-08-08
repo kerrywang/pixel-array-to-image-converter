906f84bd52d77abc18fe151981b36985d8f48497
Kaiyue Wang <kaiyuewang@berkeley.edu>
2017-08-07 19:53:51 -0700

    update version

--- conversion.py
+++ conversion.py
@@ -1,13 +1,14 @@
-from PIL import Image
 import numpy as np
 from csvReader import get_user_list
 from matplotlib import pyplot as plt
-import cv2
 
-
+newStart = 15000 #the picture number you want to start with
 num = 0
 imageList = get_user_list()
 for image in imageList:
+	if num <= newStart:
+		num += 1
+		continue
     target = np.asarray(np.split(np.asarray(image),48))
     plt.imshow(target, cmap='gray',interpolation="bicubic", vmin=0, vmax=255)
     plt.savefig("./images/No. " + str(num) + ".png", dpi = 113)
